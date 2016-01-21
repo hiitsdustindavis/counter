6984e25cf791938d2d377a0aa2be38b1850abaad
Dustin Davis and Josh Overly <hiitsdustindavis@gmail.com>
2016-01-21 11:17:23 -0800

    Add user logic, form inputs and submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
-var counter = function() {
-  var output = []
-  var loopEnd = 20;
-  var multiple = 5;
-  for (i = multiple; i <= loopEnd; i += multiple) {
-    return output.push[i];
-  };
+var counter = function(loopEnd, multiple) {
+  var output = [];
+  for(var i = multiple; i <= loopEnd; i += multiple) {
+      output.push(i);
 
+  }
+
+  return output;
 };
+
+
+$(function() {
+
+  $("form#counter").submit(function(event) {
+
+    var loopEnd = parseInt($("input#count-to").val());
+    var multiple = parseInt($("input#multiple").val());
+    debugger;
+    var numbers = counter(loopEnd, multiple);
+
+    $(".output").text(numbers);
+
+    $("#result").show();
+    event.preventDefault();
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('counter', function() {
   it("create variable that returns an end of loop", function() {
-    expect(counter("output")).to.equal(5,10,15,20);
+    expect(counter(30, 7)).to.eql([7, 14, 21, 28]);
   });
 
 });
