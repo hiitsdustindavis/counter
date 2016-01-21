cbf5b1e78d984c698a7c1044bf46125a03a53412
Dustin Davis and Josh Overly <joshoverly@yahoo.com>
2016-01-21 09:28:11 -0800

    initial commit, add looping function

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var counter = function() {
+  var output = []
+  var loopEnd = 20;
+  var multiple = 5;
+  for (i = multiple; i <= loopEnd; i += multiple) {
+    return output.push[i];
+  };
+
+};
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe('counter', function() {
+  it("create variable that returns an end of loop", function() {
+    expect(counter("output")).to.equal(5,10,15,20);
+  });
+
+});
